20ef985f61f0b60e8d7093f25f49ef44aae9ed31
ianhhhhhhhhe <517382432@qq.com>
2018-06-23 13:37:44 +0800

    remove chinese

--- bot.sql
+++ bot.sql
@@ -1,29 +1,29 @@
 CREATE TABLE user_status (
-	from_address CHAR(33) NOT NULL,  -- 设备地址
-	address CHAR(32) NOT NULL,  -- 用户签名地址
-	amount INTEGER NOT NULL,  -- 用户购买金额
-	lockupId INTEGER NOT NULL,  -- 产品ID
-	shared_address CHAR(32) NULL,  -- 用户合约地址
-	sent INTEGER NULL,  -- 是否已发送地址合约地址
+	from_address CHAR(33) NOT NULL,
+	address CHAR(32) NOT NULL,
+	amount INTEGER NOT NULL,
+	lockupId INTEGER NOT NULL,
+	shared_address CHAR(32) NULL,
+	sent INTEGER NULL,
 	PRIMARY KEY(from_address, lockupId)
 );
 
 CREATE TABLE lockups (
-	financialBenefitsId INTEGER NOT NULL PRIMARY KEY, -- 产品ID
-	productName CHAR(32) NULL,  -- 产品名称
-	activityStatus CHAR(32) NOT NULL,  -- 产品状态
-	financialId INTEGER NOT NULL,  -- 套餐ID：7天/30天/90天/···
-	financialRate INTEGER NOT NULL,  -- 利率
-	financialStatus INTEGER NOT NULL,  -- 已/未发收益
-	interestStartTime INTEGER NOT NULL,  -- 计息开始时间
-	interestEndTime INTEGER NOT NULL,  -- 计息结束时间
-	minAmount INTEGER NOT NULL,  -- 起购金额
-	purchaseLimit INTEGER NULL,  -- 限购金额
-	panicStartTime INTEGER NOT NULL,  -- 活动开始时间
-	panicEndTime INTEGER NOT NULL,  -- 活动结束时间
-	nextPanicStartTime INTEGER NULL,  -- 下期活动开始时间
-	nextPanicEndTime INTEGER NULL,  -- 下期活动结束时间
-	panicTotalLimit INTEGER NOT NULL,  -- 抢购总额度
-	remainLimit INTEGER NULL,  -- 剩余额度
-	unlockTime INTEGER NOT NULL  -- 解锁时间
+	financialBenefitsId INTEGER NOT NULL PRIMARY KEY,
+	productName CHAR(32) NULL,
+	activityStatus CHAR(32) NOT NULL,
+	financialId INTEGER NOT NULL,
+	financialRate INTEGER NOT NULL,
+	financialStatus INTEGER NOT NULL,
+	interestStartTime INTEGER NOT NULL,
+	interestEndTime INTEGER NOT NULL,
+	minAmount INTEGER NOT NULL,
+	purchaseLimit INTEGER NULL,
+	panicStartTime INTEGER NOT NULL,
+	panicEndTime INTEGER NOT NULL,
+	nextPanicStartTime INTEGER NULL,
+	nextPanicEndTime INTEGER NULL,
+	panicTotalLimit INTEGER NOT NULL,
+	remainLimit INTEGER NULL,
+	unlockTime INTEGER NOT NULL
 );
