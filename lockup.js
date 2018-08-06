554dd68e4dd2c1151a4fa8b102ac48f7aa4fef19
ianhhhhhhhhe <517382432@qq.com>
2018-08-06 16:33:31 +0800

    Push again in case server lost some data

--- lockup.js
+++ lockup.js
@@ -31,8 +31,10 @@ function prePurchaseLockup(from_address, address, amount, lockupId, maxAmount) {
 					// query database and check if client has put required amout into the address
 					// sendMessageToDevice(from_address, '你已参加过该活动，请选择其他套餐或关注下期活动');
 					network.getUserStatus('/financial-lockup/all.htm', from_address, function(result){
+						let find = false
 						result.map(function(lockup){
 							if(lockup["sharedAddress"]===rows[0]["shared_address"]){
+								find = true
 								var remain = maxAmount - parseFloat(lockup["lockUpAmount"]?lockup["lockUpAmount"]:0);
 								if(remain <= 0){
 									return sendMessageToDevice(from_address, '你已参加过该活动，请选择[其他套餐](command:锁仓激励服务)或关注下期活动')
@@ -41,6 +43,24 @@ function prePurchaseLockup(from_address, address, amount, lockupId, maxAmount) {
 								//'本次解锁后的收益为'+rows[0]["amount"]+'MN，收益需审核后返还到你的合约地址里，一般T+1到账，周末及节假日顺延');
 							}
 						})
+						if(!find) {
+							network.postUserStatus('/financial-lockup/save.htm', from_address, shared_address, account_address, lockupId, amount, function(res, error, status_code, code){
+								if (error) {
+									console.log('Error: ', error);
+									sendMessageToDevice(from_address, 'bot似乎出了点问题，请联系Trustnote工作人员,code:500');
+									return;
+								} else if (status_code) {
+									sendMessageToDevice(from_address, 'Shared_address: '+shared_address +' Status code: ' + status_code +'请[重新发起流程](command:锁仓激励服务)');
+									return;
+								}
+								if(!res){
+									console.log('Error: '+ code + '#' + info+ '#' + lockupId + '不存在');
+									return sendMessageToDevice(from_address, '服务号似乎出了点问题，请联系工作人员，错误代号:' + code + '#' + info+ '#' + lockupId + '并[重新发起流程](command:锁仓激励服务)');
+								}
+								console.log('===device_address===: '+from_address+'\'s shared address is '+shared_address)
+								sendMessageToDevice(from_address, '认证通过\n请['+amount+'MN](TTT:'+shared_address+'?amount='+amount*1000000+')以完成锁仓激励计划（请点击当前地址支付）\n\n转多或转少不计入收益，本次解锁后的收益为'+res["income_amount"]+'MN，获得TFS数量为'+res['tfans_amount']+'，收益需审核后返还到你的合约地址里，TFS返回到你的钱包地址，一般T+1到账，周末及节假日顺延')
+							});
+						}
 					});
 				}
 				return;
